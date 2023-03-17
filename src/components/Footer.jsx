aa1dd3457d653c33b6af13a3a2fc06751cdbe254
matu <salinasmateo@oulook.com>
2023-03-17 12:09:34 -0300

    starting repo w/ footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import mountain from '../assets/mountain.png'
+import logo from '../assets/boranahorizontal.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <img src={mountain} alt="mountain-borana" className='footer-img'/>
+        <div className='footer-box'>
+            <div className='footer-left'>
+                <img src={logo} alt="" />
+                <p>Tostadero de Cafe de Especialidad</p>
+                <div className='footer-logos'>
+                    <p>fb</p>
+                    <p>ig</p>
+                    <p>wpp</p>
+                </div>
+            </div>
+            <div className='footer-right'>
+                <div className='footer-menu'>
+                    <h3>Menu</h3>
+                    <p>Por que elegirnos?</p>
+                    <p>Como comprar?</p>
+                    <p>Quienes somos?</p>
+                    <p>Contacto</p>
+                </div>
+                <div className='footer-menu'>
+                    <h3>Contactanos</h3>
+                    <div>
+                        <p>Logo +54 0 2236 03-5028</p>
+                    </div>
+                    <div>
+                        <p>Logo @borana.cafe</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
