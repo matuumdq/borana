eac39e50f732039fdd0e58bb61dc68d71df0668c
unknown <msalperin@hotmail.com>
2023-03-24 12:15:37 -0300

    cambios header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
-import logo from '../assets/boranaverticalblanco.png'
-import Navbar from './Navbar'
-import Shipping from './Shipping'
+import logo from '../assets/boranaverticalblanco.png';
+import Navbar from './Navbar';
+import Shipping from './Shipping';
+import '../styles/header.css';
 
 const Header = () => {
   return (
-    <>
+
+    <header>
+
         <Shipping />
         <Navbar />
-        
-        <div className='bg-header'>
-            <img src={logo} alt="borana logo" className='header-logo'/>
-            <p className='header-toast'>Tostadores de Café</p>
+
+        <div className='slider-conteiner'>
+            <img src={logo} alt="borana logo" className='slider-logo'/>
+            <p className='slider-title'>Tostadores de Café</p>
         </div>
-    </>
+
+    </header>
   )
+
 }
 
 export default Header
